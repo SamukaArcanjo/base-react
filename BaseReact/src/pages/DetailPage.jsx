2e7ae9f8ffb8adf508efcf927c1e46b442500af2
SamukaArcanjo <samuelarcanjo57@gmail.com>
2026-07-24 11:54:16 -0300

    feat: Botão de voltar para a pagina principal no DetailPage

--- BaseReact/src/pages/DetailPage.jsx
+++ BaseReact/src/pages/DetailPage.jsx
@@ -1,3 +1,4 @@
+import { ChevronLeft } from "lucide-react";
 import { useSearchParams } from "react-router-dom";
 
 function DetailPage() {
@@ -6,10 +7,16 @@ function DetailPage() {
   const description = searchParams.get("description");
 
   return (
-    <div className="bg-[#080825] w-screen h-screen p-6 flex flex-col justify-start items-center">
-      <h1 className="text-4xl font-bold text-[#fff] text-center p-6">
-        Detalhes da Tarefa
-      </h1>
+    <div className="bg-[#080825] w-screen h-screen p-6 flex flex-col justify-start items-center gap-6">
+      <div className="w-[500px] flex justify-center relative">
+        <button className="absolute left-0 top-1/2 -translate-y-1/2  text-white">
+          <ChevronLeft />
+        </button>
+        <h1 className="text-4xl font-bold text-[#fff] text-center ">
+          Detalhes da Tarefa
+        </h1>
+      </div>
+
       <div className="w-[500px] h-[200px] rounded-md shadow bg-[#fff] space-y-4 p-6 text-center ">
         <h1 className="font-bold text-3xl">{title}</h1>
         <p className="text-2xl">{description}</p>
